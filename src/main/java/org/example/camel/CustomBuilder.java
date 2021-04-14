e0d51f4e47c1143e614716b2cea3addbc4147848
Damien PAUL <d.paul@proman-interim.com>
2021-04-14 19:59:49 +0200

    recipientList - finalisation

--- src/main/java/org/example/camel/CustomBuilder.java
+++ src/main/java/org/example/camel/CustomBuilder.java
@@ -3,19 +3,20 @@ package org.example.camel;
 import org.apache.camel.AggregationStrategy;
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
-import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomBuilder extends RouteBuilder {
 
+    private final static String LOG_PATTERN = " - ${exchangeId} -> ${routeId} -> ${exchange.getFromRouteId} -> ${body}";
+    private static final String LOG_NAME = "logging-route";
+
     @Override
     public void configure() throws Exception {
 
         var timer = "timer:scheduler";
+        var dispatch = "direct:dispatch";
 //        var route1 = "seda:route-1";
 //        var route2 = "seda:route-2";
 //        var route3 = "seda:route-3";        
@@ -24,42 +25,43 @@ public class CustomBuilder extends RouteBuilder {
         var route3 = "direct:route-3";
         var recipients = String.format("%s,%s,%s",route1,route2,route3);
 
+
         from(timer + "?period=1000000").routeId(timer)
                 .setBody(simple("TIMER"))
-                .log(LoggingLevel.INFO, "logging-route","000 - ${exchangeId} -> ${routeId} -> ${exchange.getFromRouteId} -> ${body}")
+                .log(LoggingLevel.INFO, LOG_NAME,"000" + LOG_PATTERN)
+                .to(dispatch);
+
+        from(dispatch).routeId(dispatch)
                 .recipientList(simple(recipients))
                 .aggregationStrategy(new CustomAggregationStrategy()).parallelProcessing()
-                .log(LoggingLevel.INFO, "logging-route","000 - ${exchangeId} -> ${routeId} -> ${exchange.getFromRouteId} -> ${body}")
+                .log(LoggingLevel.INFO, LOG_NAME,"010" + LOG_PATTERN)
                 .end();
 
         from(route1).routeId(route1)
                 .setBody(simple("${body} ROUTE1"))
-                .log(LoggingLevel.INFO, "logging-route","100 - ${exchangeId} -> ${routeId} -> ${exchange.getFromRouteId} -> ${body}")
-                .process(new Watch());
+                .log(LoggingLevel.INFO, LOG_NAME,"100" + LOG_PATTERN)
+                .process(new CustomProcessor());
 
         from(route2).routeId(route2)
                 .setBody(simple("${body} ROUTE2"))
-                .log(LoggingLevel.INFO, "logging-route","200 - ${exchangeId} -> ${routeId} -> ${exchange.getFromRouteId} -> ${body}")
-                .process(new Watch());
+                .log(LoggingLevel.INFO, LOG_NAME,"200" + LOG_PATTERN)
+                .process(new CustomProcessor());
 
         from(route3).routeId(route3)
                 .setBody(simple("${body} ROUTE3"))
-                .log(LoggingLevel.INFO, "logging-route","300 - ${exchangeId} -> ${routeId} -> ${exchange.getFromRouteId} -> ${body}")
-                .process(new Watch());
+                .log(LoggingLevel.INFO, LOG_NAME,"300" + LOG_PATTERN)
+                .process(new CustomProcessor());
         
     }
     
     public class CustomAggregationStrategy implements AggregationStrategy {
-
-        private final Logger LOGGER = LogManager.getLogger(CustomAggregationStrategy.class);
 
         @Override
         public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
             if (oldExchange == null) {
                 return newExchange;
             }
-
-            String body = oldExchange.getIn().getBody(String.class);
+            var body = oldExchange.getIn().getBody(String.class);
             oldExchange.getIn().setBody(body + " * " + newExchange.getIn().getBody(String.class));
             return oldExchange;
 
--- src/main/java/org/example/camel/CustomProcessor.java
+++ src/main/java/org/example/camel/CustomProcessor.java
@@ -5,16 +5,16 @@ import org.apache.camel.Processor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Watch implements Processor {
-    private final static Logger LOGGER = LogManager.getLogger(Watch.class);
+public class CustomProcessor implements Processor {
+    private final static Logger LOGGER = LogManager.getLogger(CustomProcessor.class);
     @Override
     public void process(Exchange exchange) throws Exception {
         var exchangeId = exchange.getExchangeId();
         var toEndPoint = exchange.getProperty(Exchange.TO_ENDPOINT);
         var fromRouteId = exchange.getFromRouteId();
-        var body = exchange.getIn().getBody();
+        var body = exchange.getIn().getBody() + " $";
         LOGGER.info("900 - {} -> {} -> {} -> {}", exchangeId, toEndPoint, fromRouteId, body);
-        exchange.getMessage().setBody(exchange.getIn().getBody());
+        exchange.getMessage().setBody(body);
     }
 }
 
