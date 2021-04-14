206d0f13cbd8d6ca454332b57f5e212e74df60b3
Damien PAUL <d.paul@proman-interim.com>
2021-04-14 17:23:32 +0200

    first commit

--- src/main/java/org/example/camel/CustomBuilder.java
+++ src/main/java/org/example/camel/CustomBuilder.java
@@ -10,10 +10,10 @@ public class CustomBuilder extends RouteBuilder {
     public void configure() throws Exception {
 
         var timer = "timer:scheduler";
-//        var route1 = "seda:route-1";
-//        var route2 = "seda:route-2";
-        var route1 = "direct:route-1";
-        var route2 = "direct:route-2";
+        var route1 = "seda:route-1";
+        var route2 = "seda:route-2";
+//        var route1 = "direct:route-1";
+//        var route2 = "direct:route-2";
        
         from(timer + "?period=10000").routeId(timer)
                 .setBody(simple("TIMER"))
@@ -22,8 +22,8 @@ public class CustomBuilder extends RouteBuilder {
 
         from(route1).routeId(route1)
                 .setBody(simple("${body} ROUTE1"))
-                .process(new Watch())
-                .log(LoggingLevel.INFO, "logging-route","100 - ${exchangeId} -> ${routeId} -> ${body}")                
+                .log(LoggingLevel.INFO, "logging-route","100 - ${exchangeId} -> ${routeId} -> ${body}")
+                .process(new Watch())                
                 .to(route2);
 
         from(route2).routeId(route2)
@@ -31,11 +31,6 @@ public class CustomBuilder extends RouteBuilder {
                 .log(LoggingLevel.INFO, "logging-route","200 - ${exchangeId} -> ${routeId} -> ${body}")
                 .process(new Watch())
                 .log(LoggingLevel.INFO, "logging-route","...");
-
-        var route3 = "seda:route-3";
-        var route4 = "seda:route-4";
-        var route5 = "seda:route-5";
-
 
     }
 }
